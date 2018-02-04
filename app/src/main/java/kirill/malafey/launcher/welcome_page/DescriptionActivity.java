3e7255341535f6898fa539da6340f51491fd0cf3
Kimentii <malafeikirill@mail.ru>
2018-02-04 11:16:08 +0300

    Added DescriptionActivity(with wrong content). Fixed land version of HelloActivity.

---
+++ app/src/main/java/kirill/malafey/launcher/welcome_page/DescriptionActivity.java
@@ -0,0 +1,15 @@
+package kirill.malafey.launcher.welcome_page;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kirill.malafey.launcher.R;
+
+public class DescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
